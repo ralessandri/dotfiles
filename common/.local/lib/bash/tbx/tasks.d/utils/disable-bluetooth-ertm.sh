da1996fd7844696b0079987eda5b5fac1d395c3c
René Alessandri <rene.alessandri@gmail.com>
2026-07-22 18:33:44 +0200

    fix: update bluetooth script path and remove wofi config
    - Update path to init.sh in disable-bluetooth-ertm.sh
    - Remove unused wofi tmux configuration file

--- common/.local/lib/bash/tbx/tasks.d/utils/disable-bluetooth-ertm.sh
+++ common/.local/lib/bash/tbx/tasks.d/utils/disable-bluetooth-ertm.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-source "$HOME/.dotfiles/dist-common/.local/lib/bash/init.sh"
+source "$HOME/.dotfiles/common/.local/lib/bash/init.sh"
 
 echo Y | sudo tee /sys/module/bluetooth/parameters/disable_ertm >/dev/null
 success_message "Bluetooth ERTM disabled"
