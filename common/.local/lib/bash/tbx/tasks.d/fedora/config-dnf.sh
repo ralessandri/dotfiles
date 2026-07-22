5fbe2ca64200d2caca8a439f363370c9f86bd34b
René Alessandri <rene.alessandri@gmail.com>
2026-07-22 16:54:22 +0200

    fix: update source paths to use common directory in scripts

--- common/.local/lib/bash/tbx/tasks.d/fedora/config-dnf.sh
+++ common/.local/lib/bash/tbx/tasks.d/fedora/config-dnf.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-source "$HOME/.dotfiles/home/.local/lib/bash/core/init.sh"
+source "$HOME/.dotfiles/common/.local/lib/bash/core/init.sh"
 
 print_section "Configuring  DNF..."
 sudo sed -i '/^max_parallel_downloads=/c\max_parallel_downloads=10' /etc/dnf/dnf.conf || echo 'max_parallel_downloads=10' >>/etc/dnf/dnf.conf
