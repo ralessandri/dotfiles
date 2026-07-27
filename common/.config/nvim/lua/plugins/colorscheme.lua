d3ce78c03d2d1dc03d28866451dc3876740cf9b4
René Alessandri <rene.alessandri@gmail.com>
2026-07-27 11:13:41 +0200

    chore: update terminal, window manager, and theme config
    - migrate from Alacritty to Ghostty
    - update niri layout presets and add theme template
    - switch neovim colorscheme to catppuccin-macchiato

--- common/.config/nvim/lua/plugins/colorscheme.lua
+++ common/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,27 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "macchiato",
+      transparent_background = true,
+      float = {
+        transparent = true,
+        -- solid = true,
+      },
+      color_overrides = {
+        macchiato = {
+          -- base = "#242424", -- Haupt-Hintergrundfarbe (Normal bg)
+          -- mantle = "#2B2B2B", -- etwas dunklerer Hintergrund (z. B. Sidebars)
+          -- crust = "#0f0f1c", -- dunkelster Hintergrund (z. B. Statusline-Ecken)
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
 }
