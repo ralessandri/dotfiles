64abfa4c6631bb028ef47870a62be08332a463a7
René Alessandri <rene.alessandri@gmail.com>
2026-08-24 16:27:05 +0200

    feat: add starship shell prompt support
    - Define STARSHIP_CONFIG environment variable
    - Initialize starship for bash shells

--- common/.bashrc.d/00_config.sh
+++ common/.bashrc.d/00_config.sh
@@ -1,3 +1,4 @@
 export HISTCONTROL=ignorespace:erasedups
 export EDITOR=nvim
 export WEZTERM_CONFIG_FILE="$HOME/.config/wezterm/wezterm.lua"
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
