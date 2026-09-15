578ed0b75932a4b15e6f594144a314ed202f45f1
René Alessandri <rene.alessandri@gmail.com>
2026-09-15 11:45:22 +0200

    style: update shell and editor themes
    - update PS1 colors in bash
    - switch Neovim colorscheme to catppuccin-macchiato
    - add custom directory icons to yazi flavor configuration

--- dotfiles/.config/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,26 @@
-return {}
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "macchiato",
+      transparent_background = true,
+      float = {
+        transparent = true,
+        -- solid = true, -- Use opaque backgrounds for floating windows.
+      },
+    },
+  },
+  {
+    "AvengeMedia/base46",
+    opts = {
+      transparency = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}
